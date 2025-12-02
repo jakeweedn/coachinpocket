decadbef6989cd0fe640ee40781a5e3af7be21bf
Jake <weednj@gmail.com>
2025-12-02 12:01:21 -0700

    Lunch

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -4,11 +4,57 @@ import { routeSectionsService } from '@/services/RouteSectionsService.js';
 import { routesService } from '@/services/RoutesService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
 
-const routes = computed(() => AppState.routes)
+// const routes = computed(() => AppState.routes)
+// const routeSections = computed(() => AppState.routeSections)
+
+// Above what I did before. Redoing it below... 
+
+
+
+const routes = computed(() => {
+  if (selectedWallSection.value == 'all') {
+    return AppState.routes
+
+  }
+
+  return AppState.routes.filter(route => route.wallSection == selectedWallSection.value)
+
+
+})
+
 const routeSections = computed(() => AppState.routeSections)
+
+
+
+
+
+
+
+const account = computed(() => AppState.account) //come back, do we need on this app? 
+const selectedWallSection = ref('all')
+
+
+// const wallSections = [
+//   {
+//     name: 'all',
+//     // backgroundImg: 
+
+//     name: {{ routeSection.name }}
+//     // // backgroundImg: 
+
+//   }
+
+
+// ]
+
+
+//I'm not even sure I need the above section since I already have model and schema for route section... Maybe for all, hmmm....
+
+
+
 
 onMounted(() => {
 
@@ -58,43 +104,24 @@ async function getRouteSections() {
 
 <template>
 
-  <h1 class="text-center"> Hello what's up </h1>
+
+
+  <section class="container">
+
+    <div v-for="routeSection in routeSections" :key="'filter button-' + routeSection.name">
+
+      <div @click="selectedWallSection = routeSection.name" role="button">
+        {{ routeSection.name }}
+
+      </div>
 
 
 
 
-
-
-
-
-
-  <div v-for="route in routes" :key="route.id">
-
-
-
-
-    <div v-if="route">
-      <p> {{ routes }}</p>
-
-
-      <p> {{ route.color }} </p>
-
-      <h3>{{ routeSections }} </h3>
     </div>
 
+  </section>
 
-
-
-
-
-  </div>
-
-
-  <div v-for="routeSection in routeSections" :key="routeSection.id">
-
-    <p> {{ routeSection.name }}</p>
-
-  </div>
 </template>
 
 <style scoped lang="scss"></style>
