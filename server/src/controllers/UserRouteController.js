4ecb4f24a569b2a954c9718b260f5025515e05a7
Jake <weednj@gmail.com>
2026-02-24 16:18:36 -0700

    Add UserRoute

---
+++ server/src/controllers/UserRouteController.js
@@ -0,0 +1,50 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import BaseController from "../utils/BaseController.js";
+
+
+export class UserRouteController extends BaseController {
+
+    constructor() {
+
+        super('api/userroutes')
+        this.router
+
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .post('', this.createUserRoute)
+
+
+    }
+
+    async createUserRoute(request, response, next) {
+
+        try {
+            console.log('Creating user route')
+            const userRouteData = request.body
+
+            const userInfo = request.userInfo
+            userRouteData.accountId = userInfo.id
+
+            const createdUserRoute = await userRouteService.createUserRoute(userRouteData)
+
+            response.send(createdUserRoute)
+
+
+
+
+        }
+
+        catch {
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
--- server/src/models/UserRoute.js
+++ server/src/models/UserRoute.js
@@ -5,8 +5,19 @@ export const UserRouteSchema = new Schema({
 
     accountId: { type: Schema.ObjectId, required: true, ref: 'Account' },
     //id of the user completing
-    routeId: { type: Schema.ObjectId, required: true, ref: 'Route' }
+    routeId: { type: Schema.ObjectId, required: true, ref: 'Route' },
     //id of the route that was completed
+
+    completed: { type: Boolean, default: false, required: true },
+    //We'll take the completed off later if we need to.
+    attempts: { type: Number }
+
+    // Yes it looks like this is what I should do. However, I still have two questions... 1. Why no completed? And 2. What does it mean for users to 'have' an object as Mick mentioned? How to implement this into code?? 
+
+
+
+
+
 
 
 
@@ -23,6 +34,7 @@ export const UserRouteSchema = new Schema({
 
 )
 
+//Might not really need the below virtual... see inspire video...  
 UserRouteSchema.virtual('profile', {
 
     localField: 'accountId',
